0c858862a4d0ccee7980fa2d882768365374fa66
KeonhoPark <81609381+KeonhoPark@users.noreply.github.com>
2022-07-02 00:39:52 +0900

    Add files via upload

---
+++ smallestShelter/src/main/java/com/umc_sjs/smallestShelter/SmallestShelterApplication.java
@@ -0,0 +1,13 @@
+package com.umc_sjs.smallestShelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallestShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallestShelterApplication.class, args);
+	}
+
+}
---
+++ smallestShelter/src/test/java/com/umc_sjs/smallestShelter/SmallestShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc_sjs.smallestShelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallestShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
